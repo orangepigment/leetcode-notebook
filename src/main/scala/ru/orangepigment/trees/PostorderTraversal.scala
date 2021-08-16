73bc7a8d189522060244a52eb8f39171ff634bb3
orangepigment <cadmium.orange@yandex.ru>
2021-08-16 14:44:25 +0300

    PostorderTraversal solution added

---
+++ src/main/scala/ru/orangepigment/trees/PostorderTraversal.scala
@@ -0,0 +1,31 @@
+package ru.orangepigment.trees
+
+import ru.orangepigment.data.TreeNode
+
+import scala.collection.mutable
+
+object PostorderTraversal {
+
+  // left -> right -> root
+  def postorderTraversal(root: TreeNode): List[Int] = {
+    val stack = mutable.Stack.empty[TreeNode]
+    val acc = mutable.ListBuffer.empty[Int]
+
+    var currentNode = root
+
+    while (currentNode != null) {
+      Option(currentNode.left).foreach(stack.push)
+      Option(currentNode.right).foreach(stack.push)
+      currentNode.value +=: acc
+
+      if (stack.isEmpty) {
+        currentNode = null
+      } else {
+        currentNode = stack.pop()
+      }
+    }
+
+    acc.toList
+  }
+
+}
