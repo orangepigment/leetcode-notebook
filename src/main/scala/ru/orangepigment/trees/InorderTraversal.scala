272e86643468aeabb27337a53b1845f440bdb94b
orangepigment <cadmium.orange@yandex.ru>
2021-08-16 14:17:23 +0300

    InorderTraversal solution added

---
+++ src/main/scala/ru/orangepigment/trees/InorderTraversal.scala
@@ -0,0 +1,25 @@
+package ru.orangepigment.trees
+
+import ru.orangepigment.data.TreeNode
+
+/**
+ * [[https://leetcode.com/problems/binary-tree-inorder-traversal/]]
+ */
+object InorderTraversal {
+
+  // left -> root -> right
+  def inorderTraversal(root: TreeNode): List[Int] = {
+    inorderTraversalRecursive(root)
+  }
+
+  def inorderTraversalRecursive(node: TreeNode, acc: List[Int] = List.empty[Int]): List[Int] = {
+    if (node == null) {
+      acc
+    } else {
+      Option(node.left).map(leftNode => inorderTraversalRecursive(leftNode, acc)).getOrElse(acc) :+
+        node.value :++
+        Option(node.right).map(leftNode => inorderTraversalRecursive(leftNode, acc)).getOrElse(List.empty[Int])
+    }
+  }
+
+}
