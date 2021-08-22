34c3575b9bc458264e1103b99c35dcab103459d9
orangepigment <cadmium.orange@yandex.ru>
2021-08-22 13:59:01 +0300

    InorderTraversal iterative solution added

--- src/main/scala/ru/orangepigment/trees/InorderTraversal.scala
+++ src/main/scala/ru/orangepigment/trees/InorderTraversal.scala
@@ -2,15 +2,30 @@ package ru.orangepigment.trees
 
 import ru.orangepigment.data.TreeNode
 
+import scala.collection.mutable
+
 /**
  * [[https://leetcode.com/problems/binary-tree-inorder-traversal/]]
  */
 object InorderTraversal {
 
-  // ToDo: add iterative solution
   // left -> root -> right
   def inorderTraversal(root: TreeNode): List[Int] = {
-    inorderTraversalRecursive(root)
+    val acc = mutable.ListBuffer.empty[Int]
+    val stack = mutable.Stack.empty[TreeNode]
+    var currentNode = root
+    while (stack.nonEmpty || currentNode != null) {
+      while (currentNode != null) {
+        stack.push(currentNode)
+        currentNode = currentNode.left
+      }
+
+      currentNode = stack.pop()
+      acc += currentNode.value
+
+      currentNode = currentNode.right
+    }
+    acc.toList
   }
 
   def inorderTraversalRecursive(node: TreeNode, acc: List[Int] = List.empty[Int]): List[Int] = {
