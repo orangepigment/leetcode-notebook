4acc9564f3245d854dfcf62ccc17d02bb7843f29
orangepigment <cadmium.orange@yandex.ru>
2021-08-22 09:01:47 +0300

    BinarySearch solved

---
+++ src/main/scala/ru/orangepigment/arrays/BinarySearch.scala
@@ -0,0 +1,27 @@
+package ru.orangepigment.arrays
+
+import scala.annotation.tailrec
+
+object BinarySearch {
+
+  def search(nums: Array[Int], target: Int): Int = {
+    binarySearch(nums, target)()
+  }
+
+  @tailrec
+  def binarySearch(nums: Array[Int], target: Int)(leftIndex: Int = 0, rightIndex: Int = nums.length - 1): Int = {
+    if (leftIndex <= rightIndex) {
+      val middleIndex = (leftIndex + rightIndex) / 2
+      if (target == nums(middleIndex)) {
+        middleIndex
+      } else if (target > nums(middleIndex)) {
+        binarySearch(nums, target)(middleIndex + 1, rightIndex)
+      } else {
+        binarySearch(nums, target)(leftIndex, middleIndex - 1)
+      }
+    } else {
+      -1
+    }
+  }
+
+}
