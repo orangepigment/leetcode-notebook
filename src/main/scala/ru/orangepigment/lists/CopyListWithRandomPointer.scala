583892da6272758c66ad1b8268ab822cac0fc98c
orangepigment <cadmium.orange@yandex.ru>
2021-08-22 12:44:27 +0300

    CopyListWithRandomPointer optimized

--- src/main/scala/ru/orangepigment/lists/CopyListWithRandomPointer.scala
+++ src/main/scala/ru/orangepigment/lists/CopyListWithRandomPointer.scala
@@ -1,7 +1,6 @@
 package ru.orangepigment.lists
 
-import scala.annotation.tailrec
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable
 
 /**
  * [[https://leetcode.com/problems/copy-list-with-random-pointer/]]
@@ -15,40 +14,22 @@ object CopyListWithRandomPointer {
   }
 
   def copyRandomList(head: Node): Node = {
-    // an array of original nodes
-    val arrayOfNodes = listToArray(head)
-    //  node number to number of the node pointed on by "random"
-    val randomPointers = arrayOfNodes.map { case (node, _) =>
-      arrayOfNodes.find { case (pointedNode, _) => node.random == pointedNode }
-        .map(_._2).getOrElse(-1)
+    val nodesToCopies = mutable.Map.empty[Node, Node]
+
+    def copyRecursive(node: Node): Node = {
+      if (node == null) {
+        null
+      } else {
+        val copy = new Node(node.value)
+        nodesToCopies += node -> copy
+        copy.next = nodesToCopies.getOrElse(node.next, copyRecursive(node.next))
+        copy.random = nodesToCopies.getOrElse(node.random, copyRecursive(node.random))
+
+        copy
+      }
     }
 
-    val copiedList = arrayOfNodes.map { case (node, i) =>
-      new Node(node.value) -> i
-    }
-    copiedList.foreach { case (node, i) =>
-      node.next =
-        if (i + 1 < copiedList.length) {
-          copiedList(i + 1)._1
-        } else {
-          null
-        }
-      node.random =
-        randomPointers(i) match {
-          case -1 => null
-          case j => copiedList(j)._1
-        }
-    }
-    copiedList.headOption.map(_._1).orNull
-  }
-
-  @tailrec
-  def listToArray(current: Node, acc: ArrayBuffer[Node] = ArrayBuffer.empty[Node]): Array[(Node, Int)] = {
-    if (current == null) {
-      acc.toArray.zipWithIndex
-    } else {
-      listToArray(current.next, acc :+ current)
-    }
+    copyRecursive(head)
   }
 
 }
