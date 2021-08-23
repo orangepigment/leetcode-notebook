22b987c2b48469c93b15f6f404d56fd77de1e889
orangepigment <cadmium.orange@yandex.ru>
2021-08-23 20:25:58 +0300

    HitCounter reworked

--- src/main/scala/ru/orangepigment/data/HitCounter.scala
+++ src/main/scala/ru/orangepigment/data/HitCounter.scala
@@ -1,5 +1,6 @@
 package ru.orangepigment.data
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 /**
@@ -13,20 +14,40 @@ import scala.collection.mutable
 class HitCounter() {
 
   /** Initialize your data structure here. */
-  private val timestampXHits = mutable.Map.empty[Int, Int]
+  private val timestampXHits = mutable.ArrayDeque.empty[(Int, Int)]
+  private var total = 0
 
 
   /** Record a hit.
    * @param timestamp - The current timestamp (in seconds granularity). */
   def hit(timestamp: Int): Unit = {
-    // FixME: it will be nice to add eviction
-    timestampXHits += timestamp -> (timestampXHits.getOrElse(timestamp, 0) + 1)
+    removeOldHits(timestamp)
+    timestampXHits.lastOption match {
+      case Some((t, counter)) if t == timestamp =>
+        timestampXHits.removeLast()
+        timestampXHits.append(timestamp -> (counter + 1))
+      case _ =>
+        timestampXHits.append(timestamp -> 1)
+    }
+    total += 1
+  }
+
+  @tailrec
+  private def removeOldHits(timestamp: Int): Unit = {
+    timestampXHits.headOption match {
+      case Some((t, count)) if t + 300 <= timestamp =>
+        timestampXHits.removeHead()
+        total -= count
+        removeOldHits(timestamp)
+      case _ =>
+    }
   }
 
   /** Return the number of hits in the past 5 minutes.
    * @param timestamp - The current timestamp (in seconds granularity). */
   def getHits(timestamp: Int): Int = {
-    timestampXHits.keys.filter(_ + 300 > timestamp).toList.map(timestampXHits.apply).sum
+    removeOldHits(timestamp)
+    total
   }
 
 }
