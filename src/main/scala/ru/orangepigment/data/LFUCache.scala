9a0622692167e98e96d56111c2624acee57d5c47
orangepigment <cadmium.orange@yandex.ru>
2021-08-24 00:11:54 +0300

    LFUCache draft version finished

--- src/main/scala/ru/orangepigment/data/LFUCache.scala
+++ src/main/scala/ru/orangepigment/data/LFUCache.scala
@@ -15,8 +15,8 @@ class LFUCache(_capacity: Int) {
 
   private class Node(
                       val key: Int,
-                      val value: Int,
-                      var frequency: Int,
+                      var value: Int,
+                      var frequency: Int = 1,
                       var next: Option[Node] = None,
                       var prev: Option[Node] = None,
                     )
@@ -30,7 +30,7 @@ class LFUCache(_capacity: Int) {
       case Some(node) =>
         node.frequency += 1
         removeNode(node)
-        // ToDo: Insert the node back to list
+        addNode(node)
         node.value
 
       case None => -1
@@ -38,48 +38,75 @@ class LFUCache(_capacity: Int) {
   }
 
   def put(key: Int, value: Int): Unit = {
-    cache.get(key) match {
-      case Some(node) =>
-        node.frequency += 1
-        removeNode(node)
-        // ToDo: Insert the node back to list
+    if (_capacity > 0) {
+      cache.get(key) match {
+        case Some(node) =>
+          node.value = value
+          node.frequency += 1
+          removeNode(node)
+          addNode(node)
 
-      case None =>
-        if (cache.size == _capacity) {
-          cache.remove(head.get.key)
-          removeNode(head.get)
-        }
-        val node = new Node(key, value, 1)
-        // ToDo: Add new node
-        cache.put(key, node)
+        case None =>
+          if (cache.size == _capacity) {
+            cache.remove(head.get.key)
+            removeNode(head.get)
+          }
+          val node = new Node(key, value)
+          addNode(node)
+          cache.put(key, node)
+      }
+
+      if (cache.size == _capacity && !cache.keySet.contains(key)) {
+        cache.remove(head.get.key)
+      }
     }
-
-    if (cache.size == _capacity && !cache.keySet.contains(key)) {
-      cache.remove(head.get.key)
-    }
-
   }
 
-  private def addNode(node: Node): Unit = {
-    if (head.isEmpty) {
-      head = Some(node)
-      tail = Some(node)
-    } else {
-      // ToDo: implement
-      ???
+  @annotation.tailrec
+  private def addNode(node: Node, current: Option[Node] = head): Unit = {
+    current match {
+      case None =>
+        tail match {
+          case Some(tailNode) =>
+            tailNode.next = Some(node)
+            node.prev = tail
+            tail = Some(node)
+          case None =>
+            head = Some(node)
+            tail = Some(node)
+        }
+      case Some(currentNode) =>
+        if (node.frequency < currentNode.frequency) {
+          node.next = current
+          currentNode.prev match {
+            case Some(prevNode) =>
+              prevNode.next = Some(node)
+              node.prev = currentNode.prev
+            case None =>
+              head = Some(node)
+          }
+          currentNode.prev = Some(node)
+        } else {
+          addNode(node, currentNode.next)
+        }
     }
   }
 
   private def removeNode(node: Node): Unit = {
     node.prev match {
       case Some(prevNode) => prevNode.next = node.next
-      case None => head = node.next
+      case None =>
+        head = node.next
+        head.foreach(n => n.prev = None)
     }
 
     node.next match {
       case Some(nextNode) => nextNode.prev = node.prev
       case None => tail = node.prev
     }
+
+    node.next = None
+    node.prev = None
   }
 
 }
