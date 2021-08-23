460342c5a08bc82358f82f24585e6b96968ea7c1
orangepigment <cadmium.orange@yandex.ru>
2021-08-23 21:33:22 +0300

    MergeIntervals draft added

---
+++ src/main/scala/ru/orangepigment/arrays/MergeIntervals.scala
@@ -0,0 +1,47 @@
+package ru.orangepigment.arrays
+
+import scala.annotation.tailrec
+
+/**
+ * [[https://leetcode.com/problems/merge-intervals/]]
+ */
+object MergeIntervals {
+
+  def merge(intervals: Array[Array[Int]]): Array[Array[Int]] = {
+    mergeRecursive(intervals)
+  }
+
+  @tailrec
+  def mergeRecursive(
+                      intervals: Array[Array[Int]],
+                      i: Int = 0,
+                      acc: List[Array[Int]] = List.empty[Array[Int]] // Use queue instead?
+                    ): Array[Array[Int]] = {
+
+    if (i >= intervals.length) {
+      acc.reverse.toArray
+    } else if (acc.isEmpty) {
+      mergeRecursive(
+        intervals,
+        i + 1,
+        intervals(i) +: acc
+      )
+    } else if (acc.head(1) >= intervals(i)(0)) { // FixME: intervals can be not sorted: [[1,4],[0,0]]
+      mergeRecursive(
+        intervals,
+        i + 1,
+        Array(
+          Math.min(acc.head(0), intervals(i)(0)),
+          Math.max(acc.head(1), intervals(i)(1)),
+        ) +: acc.tail
+      )
+    } else {
+      mergeRecursive(
+        intervals,
+        i + 1,
+        intervals(i) +: acc
+      )
+    }
+  }
+
+}
